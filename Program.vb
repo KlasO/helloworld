ff89dfbd61d7f971e04c56b1d4efe51b423ac2fb
Klas Olsson <klas.olsson@memoratus.se>
2021-03-07 15:19:48 +0100

    Test

--- Program.vb
+++ Program.vb
@@ -2,6 +2,9 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+            Console.WriteLine("Hello World!")
+            for i = 1 to 10
+            Console.WriteLine(i)
+            next i
     End Sub
 End Module
