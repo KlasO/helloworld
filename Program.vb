cdcf761fdfd0507d13787c7bbe20232e96b2b3c3
Klas Olsson <klas.olsson@memoratus.se>
2021-03-07 16:23:57 +0100

    -

--- Program.vb
+++ Program.vb
@@ -5,7 +5,7 @@ Module Program
     Sub Main(args As String())
             Console.WriteLine("Hello World!")
             for i = 1 to 10
-            Console.Write(i)
+            Console.Write(i & " ")
             next i
     End Sub
 End Module
