74f380dbdde7d8f2f645ac5a8beba43a1e1cbd2c
Klas Olsson <klas.olsson@memoratus.se>
2021-03-20 12:05:11 +0100

    Minor updates

--- Program.vb
+++ Program.vb
@@ -3,6 +3,20 @@ Imports System
 Module Program
 ' Test 
     Sub Main(args As String())
+            dim args_passed as Boolean
+            if args.length >0 then
+                args_passed = true
+            else 
+                args_passed = false
+            end if   
+            if args_passed then 
+                for i = 0 to args.length-1
+                    Console.WriteLine("args were passed: " & " arg(" & i & "): " & args(i))
+                next
+            else   
+                Console.WriteLine("no args were passed")
+            end if
+
             Console.WriteLine("Hello World!")
             for i = 1 to 10
             Console.Write(i & " ")
