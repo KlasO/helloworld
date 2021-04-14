24e1ddb68d5cbdb91c26d1b6aba052d81b775437
Klas Olsson <klas.olsson@memoratus.se>
2021-04-14 08:18:50 +0200

    force update of cs

--- cs/Program.cs
+++ cs/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace cs
+namespace csharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int myVar; 
+            if (args.Length = 0)
+            {
+                myVar= 1;
+                
+            }
+            else
+                {
+                    myVar= Convert.ToInt32(args[0]);
+                };
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(Math.Pow(i,i) + " ");
+                Console.WriteLine("Hello World!");
+            }
         }
     }
 }
