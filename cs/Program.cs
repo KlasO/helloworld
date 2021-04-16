594210ac090e6b445b2fe6956e362fd89ddbf772
Klas Olsson <klas.olsson@memoratus.se>
2021-04-16 14:47:16 +0200

    Test

--- cs/Program.cs
+++ cs/Program.cs
@@ -6,15 +6,14 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            int myVar; 
             if (args.Length == 0)
             {
-                myVar= 1;
+               int myVar= 1;
                 
             }
             else
                 {
-                    myVar= Convert.ToInt32(args[0]);
+                 int   myVar= Convert.ToInt32(args[0]);
                 };
             for (int i = 0; i < 4; i++)
             {
