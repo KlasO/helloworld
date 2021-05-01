eae5ae0943b1892b4c785b17fd0ab7c29655efc8
KlasO <klas.olsson@memoratus.se>
2021-05-01 13:51:53 +0200

    Simple tests

--- cs/Program.cs
+++ cs/Program.cs
@@ -10,6 +10,17 @@ namespace csharp
             if (args.Length != 0 )
             {
                    myVar= Convert.ToInt32(args[0]);
+                   if (args.Length > 1)
+                   {
+                     Console.WriteLine("Skipping the args beyond the args[0]=" + args[0]);
+                     Console.WriteLine("But for the record, these are the arguments passed");
+                    int index = 0;
+                     foreach (var item in args)
+                     {
+                     Console.WriteLine("Argument " + index + " has the value " + item);
+                     index++;
+                     }
+                   };
             };
             for (int i = 0; i < 4; i++)
             {
