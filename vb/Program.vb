4458ef6ed1739538d5b57c760b373b15ad8d62d3
KlasO <klas.olsson@memoratus.se>
2021-05-01 13:55:35 +0200

    minor vb update

--- vb/Program.vb
+++ vb/Program.vb
@@ -6,20 +6,13 @@ Module Program
             
             if args.length > 0  then 
                 for i = 0 to args.length-1
-                    Console.WriteLine("args were passed: " & " arg(" & i & "): " & args(i))
+                    Console.WriteLine("args were passed: arg(" & i & "): " & args(i))
                 next
             else   
                 Console.WriteLine("no args were passed")
             end if
 
             Console.WriteLine("Hello World!")
-            for i = 1 to 10
-                if i < 10 then 
-                Console.Write(i & " ")  
-                else
-                Console.WriteLine(i)  
-                end if
-            next i
-
+            
     End Sub
 End Module
