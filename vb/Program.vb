6916dba5198c80fa4db72a507de77701557a507c
Klas Olsson <klas.olsson@memoratus.se>
2021-11-07 14:58:47 +0100

    Update

--- vb/Program.vb
+++ vb/Program.vb
@@ -1,9 +1,7 @@
 Imports System
-
 Module Program
 ' Test 
     Sub Main(args As String())
-            
             if args.length > 0  then 
                 for i = 0 to args.length-1
                     Console.WriteLine("args were passed: arg(" & i & "): " & args(i))
@@ -11,8 +9,6 @@ Module Program
             else   
                 Console.WriteLine("no args were passed")
             end if
-
-            Console.WriteLine("Hello World!")
-            
+            Console.WriteLine(Figgle.FiggleFonts.Standard.Render("Hello, World!"))
     End Sub
 End Module
